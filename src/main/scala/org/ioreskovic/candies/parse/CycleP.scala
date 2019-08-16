fd0c1c41928b3e086f50228c524fdaead3e4a4eb
Ivan Oreskovic <ivan.oreskovic@rimac-automobili.com>
2019-08-16 22:02:42 +0200

    Fix parsing empty files

--- src/main/scala/org/ioreskovic/candies/parse/CycleP.scala
+++ src/main/scala/org/ioreskovic/candies/parse/CycleP.scala
@@ -19,7 +19,7 @@ private[parse] trait CycleP {
   private def end[_: P]: P[Unit] = P(";")
   private def s[_: P]: P[Unit] = spaceSL.rep(0)
 
-  def cycle[_: P]: P[Cycle] = P(newLine.rep ~ start ~ ident ~ msgRef ~ interval ~ end).map {
+  def cycle[_: P]: P[Cycle] = P(start ~ ident ~ msgRef ~ interval ~ end).map {
     case (msgId, duration) =>
       candies.internal.Cycle(msgId, duration)
   }
--- src/main/scala/org/ioreskovic/candies/parse/DbcP.scala
+++ src/main/scala/org/ioreskovic/candies/parse/DbcP.scala
@@ -1,7 +1,6 @@
 package org.ioreskovic.candies.parse
 
 import fastparse.MultiLineWhitespace._
-import fastparse.Parsed.Success
 import fastparse._
 import org.ioreskovic.candies.internal.Network
 
@@ -9,7 +8,7 @@ private[parse] trait DbcP {
   private def interested[_: P](n: Network): P[Network] = network.network(n)
 
   private def all[_: P](n: Network): P[Network] =
-    interested(n).rep.map(_.reduce(_ + _))
+    interested(n).rep.map(_.foldLeft(n)(_ + _))
 
-  def dbc[_: P]: P[Network] = all(Network.empty)
+  def dbc[_: P]: P[Network] = P(Start ~ all(Network.empty) ~ End)
 }
